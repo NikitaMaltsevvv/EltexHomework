ef8c054114a56b272c02c9f5856daf3ce6040145
NikitaMaltsevvv <141240049+NikitaMaltsevvv@users.noreply.github.com>
2023-11-05 17:53:51 +0700

    Task3
    Task3 completed

---
+++ task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned char num, count = 0;
+	if (scanf("%hhu", &num) != 1) return -1;
+
+	for (int i = 0; i < 8; i++, num <<= 1) (num & 0x80) ? count++ : count;
+	printf("Count of one = %hhu", count);
+
+	return 0;
+}
