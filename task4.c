d5d2c0d774a5ba72ca81b0ecc32cb844a941e3da
NikitaMaltsevvv <141240049+NikitaMaltsevvv@users.noreply.github.com>
2023-11-05 18:58:07 +0700

    Task4
    Task4 completed

---
+++ task4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void print_binary(int);
+
+int main(void)
+{
+	int num;
+	char bit;
+
+	printf("Enter number: ");
+	if (scanf("%d", &num) != 1) return -1;
+
+	print_binary(num);
+
+	printf("\nEnter value of bit: ");
+	if (scanf("%hhi", &bit) != 1) return -1;
+
+	if (bit) num |= 0x4;
+	else num &= 0xFB;
+
+	printf("Changed number = %d\n", num);
+	print_binary(num);
+
+	return 0;
+}
+
+void print_binary(int num)
+{
+	for (int i = sizeof(int) * 8 - 1; i >= 0; i--)
+	{
+		printf("%d", (num >> i) & 1);
+	}
+}
