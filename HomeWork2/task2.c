e49ce1535d1819a028ee7a92625ceb9c212a7a5e
Nikita <nikita.maltsevvv@gmail.com>
2023-11-09 19:51:26 +0700

    task1 && task2 completed

---
+++ HomeWork2/task2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define N 5
+
+int main(void)
+{
+	int arr[N];
+
+	for (int i = 0; i < N; i++) arr[i] = i + 1;
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		printf("%d", arr[i]);
+		if (i != 0) putc(' ', stdout);
+	}
+
+	return 0;
+}
