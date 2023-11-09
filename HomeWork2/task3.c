5267756b256572641551b62c08c50489d1ac5ca6
Nikita <nikita.maltsevvv@gmail.com>
2023-11-09 19:58:28 +0700

    task3 completed

---
+++ HomeWork2/task3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define N 3
+
+int main(void)
+{
+    int arr[N][N];
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            if((i + j) < 2) arr[i][j] = 0;
+            else arr[i][j] = 1;
+        }
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            printf("%d", arr[i][j]);
+            if(j != N - 1) putc(' ', stdout);
+        }
+        putc('\n', stdout);
+    }
+
+    return 0;
+}
