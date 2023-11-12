4c5b9962009952837aa7fd4936a65e778039a5c3
Nikita <nikita.maltsevvv@gmail.com>
2023-11-12 16:38:22 +0700

    HomeWork3: task2 completed + clang-format

--- HomeWork3/task1.c
+++ HomeWork3/task1.c
@@ -2,29 +2,29 @@
 
 void print_binary(int);
 
-int main(void)
-{
-    int number, temp;
-    unsigned char byte;
+int main(void) {
+  int number, temp;
+  unsigned char byte;
 
-    if((scanf("%d", &number) != 1) || (scanf("%d", &temp) != 1) || (temp > 255)) return -1;
+  if ((scanf("%d", &number) != 1) || (scanf("%d", &temp) != 1) || (temp > 255))
+    return -1;
 
-    byte = (char)temp;
-    char *pbyte = &number;
+  byte = (char)temp;
+  char *pbyte = &number;
 
-    print_binary(number);
+  print_binary(number);
 
-    *(pbyte + 2) = byte;
+  *(pbyte + 2) = byte;
 
-    putc('\n', stdout);
-    print_binary(number);
+  putc('\n', stdout);
+  print_binary(number);
 
-    return 0;
+  return 0;
 }
 
 void print_binary(int num) {
   for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
     printf("%d", (num >> i) & 1);
-    if((i % 8 == 0) && (i != 31)) putc(' ', stdout);
+    if ((i % 8 == 0) && (i != 31)) putc(' ', stdout);
   }
 }
